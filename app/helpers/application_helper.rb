fa1485bf7ec3804e51a6a597d38075cd54c1d15f
Louise Devilder <devilderlouise@gmail.com>
2017-06-19 15:42:38 +0200

    Add a nav bar home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def appropriate_navbar
     if current_page?('/')
-      # render 'shared/navbar_home'
+      render 'shared/navbar_home'
     else
       render 'shared/navbar'
     end
